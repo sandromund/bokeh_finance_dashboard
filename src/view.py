806a57031d51ff2b8aef8166c6d2755e43f1d7d4
Sandro <31536101+sandromund@users.noreply.github.com>
2023-02-18 18:58:28 +0100

    Update view.py

--- src/view.py
+++ src/view.py
@@ -7,6 +7,7 @@ from bokeh.models import ColumnDataSource
 from bokeh.models.widgets import DataTable, TableColumn
 from bokeh.palettes import Category20c
 from bokeh.plotting import figure, show
+import random
 from bokeh.transform import cumsum
 
 
@@ -17,7 +18,7 @@ class View:
         self.months = {index: month for index, month in enumerate(calendar.month_abbr) if month}
         self.table_column = []
         self.pie_column = []
-
+        self.bar_column = []
 
     def add_table_plot(self, data):
         data_table = DataTable(
@@ -48,6 +49,36 @@ class View:
         p.grid.grid_line_color = None
         self.pie_column.append(p)
 
+    def add_bar_chart(self, data: pd.DataFrame):
+
+        labels = list(data)
+        labels.remove("Month")
+
+        colors = []
+        # TODO Better generation of more distinct colors
+        r = lambda: random.randint(0, 255)
+        for _ in range(len(labels)):
+            colors.append('#%02X%02X%02X' % (r(), r(), r()))
+
+        # labels.remove("Month")
+        # source = ColumnDataSource(data=data)
+        p = figure(title="Spending by month", toolbar_location=None, tools="")
+        p.vbar_stack(labels, x='Month', width=0.9, source=data, color=colors,
+                     legend_label=labels)
+
+        p.y_range.start = 0
+        p.x_range.range_padding = 0.1
+        p.xgrid.grid_line_color = None
+        p.axis.minor_tick_line_color = None
+        p.outline_line_color = None
+        p.legend.location = "top_left"
+        p.legend.orientation = "horizontal"
+
+        self.bar_column.append(p)
+
     def show(self):
-        show(column(row(self.table_column),
-                    row(self.pie_column), sizing_mode="stretch_both"), )
+        show(
+            column(
+                row(self.table_column, sizing_mode="stretch_both"),
+                row(self.pie_column, sizing_mode="stretch_both"),
+                row(self.bar_column, sizing_mode="stretch_both")))
