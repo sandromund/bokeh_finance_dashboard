1349ce1542d20cab46a00db63f041ed3accfcad3
Sandro <31536101+sandromund@users.noreply.github.com>
2023-02-18 14:55:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
+from src.model import Model
 from src.presenter import Presenter
 from src.view import View
-from src.model import Model
 
 if __name__ == '__main__':
-    presenter = Presenter(input_csv_path="data/2020.CSV",
+    presenter = Presenter(input_csv_path="data/2022.CSV",
                           model=Model(spending_dict_path="config/spending.json"),
                           view=View())
 
