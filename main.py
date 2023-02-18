1aa2ae96fd71a82251a457752fd49e66af1a9765
Sandro <31536101+sandromund@users.noreply.github.com>
2023-02-18 19:04:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ if __name__ == '__main__':
     presenter = Presenter(input_csv_path="data/2022.CSV",
                           model=Model(spending_dict_path="config/spending.json"),
                           view=View())
-
+    # TODO December is missing in the dashboard, maybe something with the mapping went wrong because index starts at 0
     presenter.create_report()
