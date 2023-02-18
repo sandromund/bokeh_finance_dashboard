c12f62f0d3cb809474c95d9ef759d159b7d797e8
Sandro <31536101+sandromund@users.noreply.github.com>
2023-02-18 14:01:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from src.model import Model
 
 if __name__ == '__main__':
     presenter = Presenter(input_csv_path="data/2020.CSV",
-                          model=Model(),
+                          model=Model(spending_dict_path="config/spending.json"),
                           view=View())
 
     presenter.create_report()
