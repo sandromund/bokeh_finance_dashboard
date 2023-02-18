d5a8fed2d0a7d2871f830c091130876c1646b3f9
Sandro <31536101+sandromund@users.noreply.github.com>
2023-02-18 12:58:16 +0100

    Update model.py

--- src/model.py
+++ src/model.py
@@ -1,4 +1,5 @@
 import pandas as pd
+pd.options.mode.chained_assignment = None  # default='warn'
 
 
 class Model:
@@ -7,6 +8,26 @@ class Model:
         self.filters = ['Mandatsreferenz', 'Glaeubiger ID', 'Kundenreferenz (End-to-End)',
                         'Sammlerreferenz', 'Lastschrift Ursprungsbetrag', 'Auslagenersatz Ruecklastschrift',
                         'BIC (SWIFT-Code)', 'Waehrung', 'Info', 'Valutadatum']
+        self.date = "Buchungstag"
+        self.amount = "Betrag"
+
+    def preprocess_data(self, data: pd.DataFrame) -> pd.DataFrame:
+        data[self.amount] = data[self.amount].str.replace(",", ".")
+        data[self.amount] = pd.to_numeric(data[self.amount])
+        data[self.amount] = data[self.amount]
+        return data
 
     def filter_columns(self, data: pd.DataFrame) -> pd.DataFrame:
         return data.drop(columns=self.filters)
+
+    def get_income_by_month(self, data: pd.DataFrame) -> pd.Series:
+        data = data[data[self.amount] >= 0]
+        data.loc[:, self.date] = pd.to_datetime(data[self.date])
+        group = data.groupby(data[self.date].dt.month).sum().round(4)
+        return group
+
+    def get_spending_by_month(self, data: pd.DataFrame) -> pd.Series:
+        data = data[data[self.amount] < 0]
+        data.loc[:, self.date] = pd.to_datetime(data[self.date])
+        group = data.groupby(data[self.date].dt.month).sum().round(4)
+        return group
